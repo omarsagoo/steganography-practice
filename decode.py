ae431f1c9d500d4e02ac87f763ffe1d0e5824fb2
omarsagoo <om.sagoo@gmail.com>
2020-05-05 12:23:54 -0700

    [add] image encode, write text

--- decode.py
+++ decode.py
@@ -1,6 +1,5 @@
-from PIL import Image 
+from PIL import Image, ImageDraw
 
-from PIL import Image 
 def decode_image(file_location):
     encoded_image = Image.open(file_location)
     red_channel = encoded_image.split()[0]
@@ -21,5 +20,44 @@ def decode_image(file_location):
 
     decoded_image.save("images/decoded_image.png")
 
+def encode_image(text, image_path):
+    image = Image.open(image_path)
+    pixels = image.load()
+
+    x_size = image.size[0]
+    y_size = image.size[1]
+
+    create_encode_image = write_text(text, (x_size,y_size))
+    create_red_channel = create_encode_image.split()[0]
+
+    for i in range(x_size):
+        for j in range(y_size):
+            read_pixel = create_red_channel.getpixel((i,j))
+
+            if (pixels[i,j][0] | 1) == pixels[i,j][0]:
+                pixels[i,j] = (pixels[i,j][0] + 1, pixels[i,j][1], pixels[i,j][2])
+            
+            if read_pixel == 0:
+                pix = pixels[i,j]
+                pix = (pix[0] + 1, pix[1], pix[2])
+                pixels[i,j] = pix
+
+
+    image.save("new_encoded_image.png")
+                
+    
+
+def write_text(text, size):
+    image = Image.new("RGB", size, (255,255,255))
+    
+    draw = ImageDraw.Draw(image)
+    draw.text((20,20), text, fill=(0,0,0))
+
+    image.save("encode_image.png", "png")
+    return image
+
 if __name__ == "__main__":
-    decode_image("images/encoded_sample.png")
+    # decode_image("images/encoded_sample.png")
+    # encode_image("hello world! I hid this text in this image!!!!!", "images/images.jpeg")
+    decode_image("new_encoded_image.png")
+
